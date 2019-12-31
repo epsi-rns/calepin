0796f94bf0e3e75a241683c3d909814d898d0474
epsi sayidina <epsi.rns@gmail.com>
2019-12-31 21:49:31 +0700

    first commit

---
+++ pelicanconf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'epsi'
+SITENAME = 'Yet Another Static Blog'
+SITEURL = ''
+
+THEME = "tutor-01"
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Jakarta'
+
+DEFAULT_LANG = 'en'
+
+DEFAULT_PAGINATION = 10
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
