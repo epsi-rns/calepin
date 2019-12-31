ec2b0c6d3f5bd7df2200f81072f464b1e5433f10
epsi sayidina <epsi.rns@gmail.com>
2020-01-01 04:09:44 +0700

    tutor-07: with output

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,11 +3,14 @@
 from __future__ import unicode_literals
 from datetime import date
 
+import sys
+sys.path.append('.')
+
 AUTHOR = 'epsi'
 SITENAME = 'Yet Another Static Blog'
 SITEURL = ''
 
-THEME = "tutor-04"
+THEME = "tutor-07"
 
 PATH = 'content'
 
@@ -15,17 +18,21 @@ TIMEZONE = 'Asia/Jakarta'
 
 DEFAULT_LANG = 'en'
 
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = 1
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+FEED_RSS              = 'feed.xml'
+FEED_ALL_ATOM         = None
+CATEGORY_FEED_ATOM    = None
 TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+AUTHOR_FEED_ATOM      = None
+AUTHOR_FEED_RSS       = None
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+INDEX_URL             = 'blog/'
+INDEX_SAVE_AS         = 'blog/index.html'
 
 ARTICLE_URL           = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS       = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
@@ -49,4 +56,41 @@ AUTHOR_SAVE_AS        = 'author/{slug}.html'
 
 CURRENTYEAR = date.today().year
 
+# Plugins
 
+from plugins.jinja2content import jinja2content
+
+PLUGINS = [
+    # ...
+    "plugins.jinja2content",
+]
+
+# Filter
+# https://linkpeek.com/blog/how-to-add-a-custom-jinja-filter-to-pelican.html
+
+
+
+import lib.libfilter
+JINJA_FILTERS = {
+  'shuffle'    : lib.libfilter.filter_shuffle,
+  'split'      : lib.libfilter.filter_split,
+  'navigation' : lib.libfilter.filter_navigation,
+  'keyjoin'    : lib.libfilter.filter_keyjoin,
+}
+
+# Data
+
+# Blogroll: Helper for friends widget
+from lib.friends import *
+from lib.archives_gitlab import *
+from lib.archives_github import * 
+from lib.archives_pelican import *
+
+# Opengraph
+OG_LOCALE = "en_US"
+OG_LOGO   = "/assets/images/logo-gear-opengraph.png"
+
+# Service
+
+# GOOGLE_ANALYTIC_KEY = ""
+# DISQUS_KEY = ""
